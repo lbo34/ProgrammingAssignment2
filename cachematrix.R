25e904fa22c4e9db7469aa0c58fca9bfd0bff3bd
lbo34 <laurent.barcelo@lafargeholcim.com>
2016-05-19 17:13:18 -0400

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These are the functions for week 3 programming assignment
+## functions inverse a matrix but first check if the inverse has already been computed.
 
-## Write a short comment describing this function
+## This functions just chaches the inverse of the matrix in the im variable.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  im<-NULL
+  set <- function(y) {
+    x <<- y
+    im <<- NULL
+  }
+  get <- function() x
+  setim<-function(inversed) im<<-inversed
+  getim<-function() im
+  list(set=set, get=get, setim=setim, getim=getim)
 }
 
 
-## Write a short comment describing this function
+## this function checks if the result of getim is null before making the inversion, otherwise takes cached data
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  im<-x$getim()
+  if(!is.null(im)) {
+    message("getting cached data")
+    return(im)
+  }
+  mymat <- x$get()
+  im <- solve(mymat)
+  x$setim(im)
+  im
 }
